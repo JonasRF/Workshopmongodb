12c2c288e25980c10f463ccafc23baf0338c714a
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-10-31 14:24:30 -0300

    Project created

---
+++ src/main/java/com/devsuperior/WorksShopmongo/WorksShopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.WorksShopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksShopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksShopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/WorksShopmongo/WorksShopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.WorksShopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksShopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

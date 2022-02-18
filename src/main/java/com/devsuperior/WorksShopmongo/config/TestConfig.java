bce29927fbd345db3c834d57217ffe1e8f2e5d0a
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-02-18 18:03:58 -0300

    Database seeding

--- src/main/java/com/devsuperior/WorksShopmongo/config/TestConfig.java
+++ src/main/java/com/devsuperior/WorksShopmongo/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.WorksShopmongo.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.devsuperior.WorksShopmongo.models.embedded.Author;
+import com.devsuperior.WorksShopmongo.models.embedded.Comment;
+import com.devsuperior.WorksShopmongo.models.entities.Post;
 import com.devsuperior.WorksShopmongo.models.entities.User;
+import com.devsuperior.WorksShopmongo.repositories.PostRepository;
 import com.devsuperior.WorksShopmongo.repositories.UserRepository;
 
 @Configuration
@@ -18,15 +23,34 @@ public class TestConfig {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@PostConstruct
 	public void init() {
 		
 		repository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "maria Brow", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		repository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, Instant.parse("2021-02-13T11:15:01Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new Author(maria));
+		Post post2 = new Post(null, Instant.parse("2021-02-14T10:05:49Z"), "Bom dia", "Acordei feliz hoje!", new Author(maria));
+
+		Comment c1 = new Comment("Boa viagem mano!", Instant.parse("2021-02-13T14:30:01Z"), new Author(alex));
+		Comment c2 = new Comment("Aproveite", Instant.parse("2021-02-13T15:38:05Z"), new Author(bob));
+		Comment c3 = new Comment("Tenha um ótimo dia!", Instant.parse("2021-02-14T12:34:26Z"), new Author(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		repository.save(maria);
 	}
 }
--- src/main/java/com/devsuperior/WorksShopmongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/WorksShopmongo/repositories/PostRepository.java
@@ -3,9 +3,9 @@ package com.devsuperior.WorksShopmongo.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.devsuperior.WorksShopmongo.models.entities.User;
+import com.devsuperior.WorksShopmongo.models.entities.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }

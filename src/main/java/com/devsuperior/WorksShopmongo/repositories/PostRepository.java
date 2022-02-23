6095fb77e37ac5dea07193e510c5833f355a684a
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-02-23 17:20:51 -0300

    GET Post full search

--- src/main/java/com/devsuperior/WorksShopmongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/WorksShopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.WorksShopmongo.repositories;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -16,4 +17,39 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 
+	@Query("{ \r\n"
+			+ "	$and: [ \r\n"
+			+ "		{ \r\n"
+			+ "			moment: {\r\n"
+			+ "				$gte: ?1\r\n"
+			+ "			} \r\n"
+			+ "		}, \r\n"
+			+ "		{ 	\r\n"
+			+ "			moment: { \r\n"
+			+ "				$lte: ?2} \r\n"
+			+ "		} , \r\n"
+			+ "		{ 	\r\n"
+			+ "			$or: [ \r\n"
+			+ "				{ \r\n"
+			+ "					'title': { \r\n"
+			+ "						$regex: ?0, \r\n"
+			+ "						$options: 'i' \r\n"
+			+ "					} \r\n"
+			+ "				}, \r\n"
+			+ "				{ \r\n"
+			+ "					'body': { \r\n"
+			+ "						$regex: ?0, \r\n"
+			+ "						$options: 'i' \r\n"
+			+ "					} \r\n"
+			+ "				}, \r\n"
+			+ "				{ \r\n"
+			+ "					'comments.text': { \r\n"
+			+ "						$regex: ?0, $options: 'i' \r\n"
+			+ "					} \r\n"
+			+ "				} \r\n"
+			+ "			] \r\n"
+			+ "		} \r\n"
+			+ "	] \r\n"
+			+ "}")
+	List<Post> fullSearch(String text, Instant startMoment, Instant endMoment);
 }
